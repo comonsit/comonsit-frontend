e1314867db078c6e169ce89ee22c9b30b3ee3486
mauricioinaz <mauricio.glez@gmail.com>
2019-11-06 16:16:47 -0500

    Add Home Container and react redux, router dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Redirect, Route, Switch} from "react-router-dom";
+// import Layout from './hoc/Layout/Layout'
+import homeContainer from './containers/HomeContainer/HomeContainer';
+// import login from './containers/Login/Login';
+// import contacto from './containers/Contacto/Contacto';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+          {/*<Route path="/contacto" component={contacto}/>*/}
+          {/*<Route path="/login" component={login}/>*/}
+          <Route path="/" exact component={homeContainer}/>
+          {/* TODO: make 404 in redirect */}
+          <Redirect to="/"/>
+      </Switch>
+    </>
   );
 }
 
---
+++ src/containers/HomeContainer/HomeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import cl from './HomeContainer.module.css';
+
+function HomeContainer() {
+  return (
+    <div className={cl.Home}>
+      Comon Sit Cateltic
+    </div>
+  );
+}
+
+export default HomeContainer;
