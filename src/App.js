d6e622f7db546953d957097463db0d32607d00d8
mauricioinaz <mauricio.glez@gmail.com>
2019-11-13 11:25:32 -0500

    Add Conocenos, Origen and Publicaciones templates

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Redirect, Route, Switch} from "react-router-dom";
 import Layout from './hoc/Layout/Layout'
 import homeContainer from './containers/General/HomeContainer/HomeContainer';
 import conocenos from './containers/General/Conocenos/Conocenos';
+import origen from './containers/General/Origen/Origen';
+import publicaciones from './containers/General/Publicaciones/Publicaciones';
 // import login from './containers/Login/Login';
 // import contacto from './containers/Contacto/Contacto';
 
@@ -13,6 +15,8 @@ function App() {
       <Switch>
           {/*<Route path="/contacto" component={contacto}/>*/}
           <Route path="/conocenos" component={conocenos}/>
+          <Route path="/origen" component={origen}/>
+          <Route path="/publicaciones" component={publicaciones}/>
           <Route path="/" exact component={homeContainer}/>
           {/* TODO: make 404 in redirect */}
           <Redirect to="/"/>
--- src/containers/General/Origen/Origen.js
+++ src/containers/General/Origen/Origen.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import cl from './Conocenos.module.css';
+import cl from './Origen.module.css';
 
-function Conocenos() {
+function Origen() {
   return (
     <div className={cl.Paragraph}>
-      <h3>Conócenos</h3>
+      <h3>Nuestro Orígen</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
   );
 }
 
-export default Conocenos;
+export default Origen;
