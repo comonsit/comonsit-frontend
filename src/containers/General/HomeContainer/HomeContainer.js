e6ce180a6400cb5856b02e27ff07211d71a33a6d
mauricioinaz <mauricio.glez@gmail.com>
2019-11-13 11:13:39 -0500

    create lorem-ipsum home

--- src/containers/General/HomeContainer/HomeContainer.js
+++ src/containers/General/HomeContainer/HomeContainer.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import cl from './HomeContainer.module.css';
 
+import HomeImage from '../../../assets/photos/homeImage.jpg'
+
 function HomeContainer() {
   return (
     <div className={cl.Home}>
-      Comon Sit Cateltic
+      <div className={cl.Image}>
+        <img src={HomeImage} alt="Imagen Inicial"/>
+      </div>
+      <div className={cl.Paragraph}>
+        <h3>Microfinanciera Tseltal</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
     </div>
   );
 }
