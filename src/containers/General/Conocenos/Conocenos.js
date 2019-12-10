d75d66e5f4cba352818cfbdc66b529379315495f
mauricioinaz <mauricio.glez@gmail.com>
2019-12-10 18:15:57 -0500

    RE: add iframe title

--- src/containers/General/Conocenos/Conocenos.js
+++ src/containers/General/Conocenos/Conocenos.js
@@ -9,6 +9,7 @@ function Conocenos() {
       <p><FormattedMessage id="contacto.text"/></p>
       <iframe
         className={cl.Calendario}
+        title='Calendario Temporal'
         src="https://calendar.google.com/calendar/b/1/embed?height=600&amp;wkst=2&amp;bgcolor=%23bbc392&amp;ctz=America%2FDetroit&amp;src=ZzcyZ3ZvYWszOGozbWY3dmZtcG9jaWFwN2NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZXMubWV4aWNhbiNob2xpZGF5QGdyb3VwLnYuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%236633CC&amp;color=%23227F63&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0"
         width="800"
         height="600"
