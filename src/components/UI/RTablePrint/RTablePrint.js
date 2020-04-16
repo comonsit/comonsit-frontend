27630573773a0080718c7dc16d7b71b0115ea627
mauricioinaz <mauricio.glez@gmail.com>
2020-04-16 09:17:07 -0400

    RE:RTablePrint: Add monthly debt table.

---
+++ src/components/UI/RTablePrint/RTablePrint.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import classes from './RTablePrint.module.css'
+import { useTable } from 'react-table'
+
+const RTablePrint = ({ columns, data, central }) => {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data,
+  })
+
+  // Render the UI for your table
+  return (
+    <table className={classes.Table}  {...getTableProps()}>
+      <thead className={classes.TableHead}>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody className={classes.TableHead} {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          const rowClass = i===central ? classes.Central : null
+          return (
+            <tr className={rowClass} {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+export default RTablePrint
--- src/containers/Panel/Creditos/ContratoDetail/ContratoDetail.js
+++ src/containers/Panel/Creditos/ContratoDetail/ContratoDetail.js
@@ -3,10 +3,12 @@ import { FormattedMessage } from 'react-intl';
 import classes from './ContratoDetail.module.css'
 
 import TextElement from '../../../../components/UI/TextElement/TextElement';
+import RTablePrint from '../../../../components/UI/RTablePrint/RTablePrint';
 
 
 const contratoDetail = props => {
-  const items1 = ["folio", "fecha_inicio", "clave_socio", "nombres", "comunidad", "region", "area_proceso"]
+
+  const items1 = ["folio", "fecha_inicio", "clave_socio", "nombres", "comunidad", "region", "proceso"]
   const items1Array = items1.map(id => {
     return (<TextElement
               label={id}
@@ -23,31 +25,87 @@ const contratoDetail = props => {
               />)
   })
 
+  const columns = [
+          {
+            Header: "Jayeb Sc'ahc'alel (Plazo)",
+            accessor: 'plazo',
+          },
+          {
+            Header: "Smajanel (Préstamo)",
+            accessor: 'monto',
+          },
+          {
+            Header: "Bin sjol smajanel te taqu'in ta jun jun uh (Interés Mensual)",
+            accessor: 'tasa',
+          },
+          {
+            Header: "Stojel sjol taqu'in (Intereses)",
+            accessor: 'interes',
+          },
+          {
+            Header: "Stojel spisil majanbil taqu'in (Total a Pagar)",
+            accessor: 'total',
+          },
+        ]
+
+  // CREATE TABLE DATA
+  const data = []
+  let element = {}
+  let  interes_ord, interes_mor, plazo_mor
+  for (let i=1; i<= props.contrato.plazo*2; i++) {
+    interes_ord = props.contrato.monto*(props.contrato.tasa/100)*i
+    if (i>props.contrato.plazo) {
+      plazo_mor = 1
+      interes_mor = props.contrato.monto*(props.contrato.tasa/100)*i
+    } else {
+      plazo_mor = 0
+      interes_mor = 0
+    }
+    interes_mor = i>props.contrato.plazo ?  props.contrato.monto*(props.contrato.tasa/100)*i : 0
+    element = {
+      monto: props.contrato.monto,
+      tasa: parseInt(props.contrato.tasa) + plazo_mor,
+      plazo: i,
+      interes: interes_ord,
+      total: parseInt(props.contrato.monto) + interes_ord + interes_mor,
+    }
+    data.push(element)
+  }
+
   return (
     <div className={classes.Container}>
       <div className={classes.Title}>
-        <h2><FormattedMessage id="contratoDetail.title"/></h2>
+        <h2>
+          <FormattedMessage id="contratoDetail.title"/>
+        </h2>
       </div>
       <div className={classes.SubSection}>
         <div className={classes.SubTitle}>
-          <h3><FormattedMessage id="contratoDetail.datos"/></h3>
+          <h3>
+            <FormattedMessage id="contratoDetail.datos"/>
+          </h3>
         </div>
         {items1Array}
       </div>
       <div className={classes.SubSection}>
         <div className={classes.SubTitle}>
-          <h3><FormattedMessage id="contratoDetail.especificaciones"/></h3>
+          <h3>
+            <FormattedMessage id="contratoDetail.especificaciones"/>
+          </h3>
         </div>
         {items2Array}
       </div>
       <div className={classes.SubSection}>
         <div className={classes.SubTitle}>
-          <h3><FormattedMessage id="contratoDetail.informacion"/></h3>
+          <h3>
+            <FormattedMessage id="contratoDetail.informacion"/>
+          </h3>
         </div>
-        <p>----- desglose -----</p>
-        <p>----- desglose -----</p>
-        <p>----- desglose -----</p>
-        <p>----- desglose -----</p>
+        <RTablePrint
+          columns={columns}
+          data={data}
+          central={props.contrato.plazo-1}
+        />
         <p>----- gráfica -----</p>
         <p>----- gráfica -----</p>
         <p>----- gráfica -----</p>
