60d4bf6efa853fabe1fd8f9c80435c1d64003618
mauricioinaz <mauricio.glez@gmail.com>
2020-05-11 16:28:08 -0400

    RE:Currency: Option to hide if Zero value and omit negative zero sign.

--- src/components/UI/Formatting/Currency.js
+++ src/components/UI/Formatting/Currency.js
@@ -2,17 +2,21 @@ import React from 'react'
 import {FormattedNumber, IntlProvider} from 'react-intl';
 
 const currency = props => {
-  if (isNaN(props.value)) {
+  if (isNaN(props.value) || (props.hideZero && parseInt(props.value) === 0)) {
     return ''
   } else {
+    // hide negative zeroes
+    const val = (props.value === 0) ? Math.abs(props.value) : (props.value)
     return (<IntlProvider locale='en'>
               <FormattedNumber
-                value={props.value}
+                value={val}
                 style="currency"
                 currency="USD"
               />
             </IntlProvider>)
   }
+
+
 }
 
 export default currency
