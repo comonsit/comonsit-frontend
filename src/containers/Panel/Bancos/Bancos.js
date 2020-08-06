9cca12c1aad08cd6547205f8a897473f716e41df
mauricioinaz <mauricio.glez@gmail.com>
2020-08-06 16:42:37 -0400

    RE:BANCOS: Add range dates form

--- src/containers/Panel/Bancos/Bancos.js
+++ src/containers/Panel/Bancos/Bancos.js
@@ -7,15 +7,52 @@ import BancosList from './BancosList/BancosList'
 import BancosSaldos from './BancosSaldos/BancosSaldos'
 import Card from '../../../components/UI/Card/Card';
 import Button from '../../../components/UI/Button/Button';
+import Input from '../../../components/UI/Input/Input';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 import Title from '../../../components/UI/Title/Title';
 import classes from './Bancos.module.css'
+import { updateObject } from '../../../store/reducers/utility'
+import { checkValidity } from '../../../utilities/validity'
 // import * as actions from '../../../store/actions'
 import axios from '../../../store/axios-be.js'
 
 class Tsumbalil extends Component {
   state = {
     registros: [],
-    saldos: []
+    saldos: [],
+    formIsValid: false,
+    loadingReg: true,
+    loadingSaldos: true,
+    form: {
+      initialDate: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'date'
+        },
+        label: (<><FormattedMessage id="banco.initialDate"/>*</>),
+        value: '',
+        validation: {
+          required: false
+        },
+        valid: false,
+        touched: false,
+        hide: false
+      },
+      finalDate: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'date'
+        },
+        label: (<><FormattedMessage id="banco.finalDate"/>*</>),
+        value: '',  //.toISOString().substring(0, 10),
+        validation: {
+          required: false
+        },
+        valid: false,
+        touched: false,
+        hide: false
+      },
+    }
   }
 
   componentDidMount () {
@@ -26,24 +63,94 @@ class Tsumbalil extends Component {
     const authData = {
       headers: { 'Authorization': `Bearer ${this.props.token}` }
     }
-    axios.get('/registros-contables.json', authData)
+    let stDate = ''
+    if (this.state.form.initialDate.value) {
+      const tmpStartDate = new Date(this.state.form.initialDate.value)
+      stDate = `initialDate=${tmpStartDate.toISOString().substring(0, 10)}`
+    }
+    let endDate = ''
+    if (this.state.form.finalDate.value) {
+      const tmpEndDate = new Date(this.state.form.finalDate.value)
+      endDate = `?finalDate=${tmpEndDate.toISOString().substring(0, 10)}`
+    }
+
+    this.setState({
+      loadingReg: true,
+      loadingSaldos: true
+    })
+    axios.get('/registros-contables.json/?'+stDate+'&'+endDate , authData)
       .then(response => {
-        this.setState({ registros: response.data})
+        this.setState({
+          registros: response.data,
+          loadingReg: false,
+        })
       })
       .catch(error => {
+        this.setState({
+          loadingReg: false,
+        })
         // TODO: FALTA!!
       })
 
-    axios.get('/banco/saldos/', authData)
+    axios.get('/banco/saldos/?'+stDate+'&'+endDate, authData)
       .then(response => {
-        this.setState({ saldos: response.data})
+        this.setState({
+          saldos: response.data,
+          loadingSaldos: false
+        })
       })
       .catch(error => {
+        this.setState({
+          loadingSaldos: false,
+        })
         // TODO: FALTA!!
       })
   }
 
+  onSubmitForm = (event) => {
+    event.preventDefault();
+    this.onGetRegistros()
+  }
+
+  inputChangedHandler = (event, inputIdentifier) => {
+
+    const updatedFormElement = updateObject(this.state.form[inputIdentifier], {
+        value: event.target.value,
+        valid: checkValidity(event.target.value, this.state.form[inputIdentifier].validation),
+        touched: true
+    })
+
+    let updatedForm = updateObject(this.state.form, {
+        [inputIdentifier]: updatedFormElement
+    })
+
+    let formIsValid = true
+    for (let inputIds in updatedForm) {
+        formIsValid = updatedForm[inputIds].valid && formIsValid
+    }
+
+    this.setState({form: updatedForm, formIsValid: formIsValid})
+  }
+
   render () {
+
+    let saldosTable, registroTable = <Spinner/>
+    if (!this.state.loadingReg) {
+      registroTable = (
+        <BancosList
+          data={this.state.registros}
+          onClick={() => {}}
+        />
+      )
+    }
+    if (!this.state.loadingSaldos) {
+      saldosTable = (
+        <BancosSaldos
+          data={this.state.saldos}
+          onClick={() => {}}
+        />
+      )
+    }
 
     return (
       <>
@@ -57,18 +164,57 @@ class Tsumbalil extends Component {
             </Button>
           </Title>
           <div className={classes.CardsContainer}>
-            <Card title={"banco.registros"}>
-              <BancosList
-                data={this.state.registros}
-                onClick={() => {}}
-              />
-            </Card>
-            <Card title={"banco.totales"}>
-              <BancosSaldos
-                data={this.state.saldos}
-                onClick={() => {}}
-              />
-            </Card>
+            <div>
+              <Card title={"banco.fechas"}>
+                <div label="movimientos.buscarSocio">
+                  <form className={classes.Form} onSubmit={this.onSubmitForm}>
+                    <div className={classes.Inputs}>
+                      <Input
+                        label={this.state.form.initialDate.label}
+                        key= {'bancoFormInput1'}
+                        elementType={this.state.form.initialDate.elementType}
+                        elementConfig={this.state.form.initialDate.elementConfig}
+                        value={this.state.form.initialDate.value}
+                        shouldValidate={this.state.form.initialDate.validation}
+                        invalid={!this.state.form.initialDate.valid}
+                        touched={this.state.form.initialDate.touched}
+                        disabled={this.props.loading}
+                        hide={this.state.form.initialDate.hide}
+                        changed={(event) => this.inputChangedHandler(event, 'initialDate')}
+                        focused
+                        />
+                      <Input
+                        label={this.state.form.finalDate.label}
+                        key= {'bancoFormInput2'}
+                        elementType={this.state.form.finalDate.elementType}
+                        elementConfig={this.state.form.finalDate.elementConfig}
+                        value={this.state.form.finalDate.value}
+                        shouldValidate={this.state.form.finalDate.validation}
+                        invalid={!this.state.form.finalDate.valid}
+                        touched={this.state.form.finalDate.touched}
+                        disabled={this.props.loading}
+                        hide={this.state.form.finalDate.hide}
+                        changed={(event) => this.inputChangedHandler(event, 'finalDate')}
+                        />
+                    </div>
+                    <Button
+                      btnType="Success">
+                      <FormattedMessage id="banco.buscar"/>
+                    </Button>
+                  </form>
+                </div>
+              </Card>
+            </div>
+            <div>
+              <Card title={"banco.totales"}>
+                {saldosTable}
+              </Card>
+            </div>
+            <div className={classes.longCard}>
+              <Card title={"banco.registros"}>
+                {registroTable}
+              </Card>
+            </div>
           </div>
         </div>
       </>
