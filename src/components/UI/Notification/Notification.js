610e3819e847a30d3d3f7968e289f060ed781ab8
mauricioinaz <mauricio.glez@gmail.com>
2020-08-17 16:13:13 -0400

    RE:Notification: Create Notification component

---
+++ src/components/UI/Notification/Notification.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import classes from './Notification.module.css';
+
+
+const notification = (props) =>  {
+  if (props.number > 0) {
+    return (
+      <div className={classes.NotContainer}>
+        {props.children}
+        <span>{props.number}</span>
+      </div>
+    )
+  } else {
+    return <>{props.children}</>
+  }
+}
+
+export default notification;
+
+//
