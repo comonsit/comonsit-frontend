6f15606b6358990fcc63c5bd449ce3e7135d4998
mauricioinaz <mauricio.glez@gmail.com>
2020-09-28 16:58:02 -0400

    RE/DJ: Fix Sandbox Map

--- src/containers/Panel/Mapas/Sandbox/Sandbox.js
+++ src/containers/Panel/Mapas/Sandbox/Sandbox.js
@@ -13,121 +13,22 @@ import {
 import axios from '../../../../store/axios-be.js';
 import { connect } from 'react-redux';
 import classes from './Sandbox.module.scss'
-import L from 'leaflet'
-import r1 from "../../../../assets/Icons/Regions/placeholder.1.svg";
-import r2 from "../../../../assets/Icons/Regions/placeholder.2.svg";
-import r3 from "../../../../assets/Icons/Regions/placeholder.3.svg";
-import r4 from "../../../../assets/Icons/Regions/placeholder.4.svg";
-import r5 from "../../../../assets/Icons/Regions/placeholder.5.svg";
-import r6 from "../../../../assets/Icons/Regions/placeholder.6.svg";
-import r7 from "../../../../assets/Icons/Regions/placeholder.7.svg";
-import r8 from "../../../../assets/Icons/Regions/placeholder.8.svg";
-import r9 from "../../../../assets/Icons/Regions/placeholder.9.svg";
-import r10 from "../../../../assets/Icons/Regions/placeholder.10.svg";
+import icons from './icons'
+
 import regionData from '../poligons'
 const { Overlay } = LayersControl
 
 const center = [17.17, -92.0]
 
-const icons = {
-  '1': new L.Icon({
-    iconUrl:  r1,
-    iconRetinaUrl: r1,
-    iconAnchor: [5, 55],
-    popupAnchor: [10, -44],
-    iconSize: [25, 55],
-    shadowSize: [68, 95],
-    shadowAnchor: [20, 92],
-  }),
-  '2': new L.Icon({
-    iconUrl:  r2,
-    iconRetinaUrl: r2,
-    iconAnchor: [5, 55],
-    popupAnchor: [10, -44],
-    iconSize: [25, 55],
-    shadowSize: [68, 95],
-    shadowAnchor: [20, 92],
-  }),
-  '3': new L.Icon({
-    iconUrl:  r3,
-    iconRetinaUrl: r3,
-    iconAnchor: [5, 55],
-    popupAnchor: [10, -44],
-    iconSize: [25, 55],
-    shadowSize: [68, 95],
-    shadowAnchor: [20, 92],
-  }),
-  '4': new L.Icon({
-    iconUrl:  r4,
-    iconRetinaUrl: r4,
-    iconAnchor: [5, 55],
-    popupAnchor: [10, -44],
-    iconSize: [25, 55],
-    shadowSize: [68, 95],
-    shadowAnchor: [20, 92],
-  }),
-  '5': new L.Icon({
-    iconUrl:  r5,
-    iconRetinaUrl: r5,
-    iconAnchor: [5, 55],
-    popupAnchor: [10, -44],
-    iconSize: [25, 55],
-    shadowSize: [68, 95],
-    shadowAnchor: [20, 92],
-  }),
-  '6': new L.Icon({
-    iconUrl:  r6,
-    iconRetinaUrl: r6,
-    iconAnchor: [5, 55],
-    popupAnchor: [10, -44],
-    iconSize: [25, 55],
-    shadowSize: [68, 95],
-    shadowAnchor: [20, 92],
-  }),
-  '7': new L.Icon({
-    iconUrl:  r7,
-    iconRetinaUrl: r7,
-    iconAnchor: [5, 55],
-    popupAnchor: [10, -44],
-    iconSize: [25, 55],
-    shadowSize: [68, 95],
-    shadowAnchor: [20, 92],
-  }),
-  '8': new L.Icon({
-    iconUrl:  r8,
-    iconRetinaUrl: r8,
-    iconAnchor: [5, 55],
-    popupAnchor: [10, -44],
-    iconSize: [25, 55],
-    shadowSize: [68, 95],
-    shadowAnchor: [20, 92],
-  }),
-  '9': new L.Icon({
-    iconUrl:  r9,
-    iconRetinaUrl: r9,
-    iconAnchor: [5, 55],
-    popupAnchor: [10, -44],
-    iconSize: [25, 55],
-    shadowSize: [68, 95],
-    shadowAnchor: [20, 92],
-  }),
-  '10': new L.Icon({
-    iconUrl:  r10,
-    iconRetinaUrl: r10,
-    iconAnchor: [5, 55],
-    popupAnchor: [10, -44],
-    iconSize: [25, 55],
-    shadowSize: [68, 95],
-    shadowAnchor: [20, 92],
-  }),
-}
-
 const getRegionCom = (region, comunidades) => {
   return comunidades.filter(it => it.ubicacion !== null && it.region === region).map(it => {
             return (
-              <Marker position={it.ubicacion} icon={icons[it.region]}>
+              <Marker key={it.id} position={it.ubicacion} icon={icons[it.region]}>
                 <Popup>
-                {it.nombre_de_comunidad} <br /> Región {it.nombre_region} <br/> Ermita  {it.ermita} <br/> LocInegi: {it.inegiLocalidad}
+                <p>Comunidad: <strong>{it.nombre_de_comunidad}</strong>  </p>
+                <p>Región: <strong>{it.nombre_region}</strong> </p>
+                <p>Ermita  <strong>{it.ermita}</strong> </p>
+                <p>LocInegi: <strong>{it.inegiLocalidad}</strong></p>
                 </Popup>
               </Marker>
             )
@@ -145,17 +46,18 @@ const getRegionCom = (region, comunidades) => {
 const Sandbox = (props) => {
   const [localidades, setLocalidades] = useState(null)
   const [localSinGeo, setLocalSinGeo] = useState(null)
+  const [localSinErmita, setLocalSinErmita] = useState(null)
   const [regiones, setRegiones] = useState(null)
+  const [cuenta, setCuenta] = useState({total: 0, asignadas: 0, sinErmita: 0, sinInegi: 0})
 
   useEffect(() => {
-
     if (props.comunidades && props.comunidades.length > 0 &&
       props.regiones && props.regiones.length > 0
     ) {
 
       const localidadesWithGeo = props.regiones.map(region => {
         return (
-          <Overlay checked name={`Region ${region.nombre_de_region}`}>
+          <Overlay key={region.id} checked name={`Region ${region.nombre_de_region}`}>
             <LayerGroup>
               {getRegionCom(region.id, props.comunidades)}
             </LayerGroup>
@@ -163,13 +65,21 @@ const Sandbox = (props) => {
         )
       })
 
-      const localidadesNOGeo = props.comunidades.filter(it => it.ubicacion === null).map(it => {
+      const localidadesNOGeo = props.comunidades.filter(it => it.ermita !== null && it.ubicacion === null).map(it => {
         if (it.ubicacion === null) {
           return (
             <div className={classes.DataContainer}>
-              <h4>Comunidad {it.nombre_de_comunidad}</h4>
-              <p className={classes.DataContainer_Texto}>Región {it.nombre_region}</p>
-              <p className={classes.DataContainer_Texto}>Ermita  {it.ermita}</p>
+              <h5>Comunidad {it.nombre_de_comunidad} - Región {it.nombre_region} - Ermita  {it.ermita}</h5>
+            </div>
+          )
+        }
+        return null
+      })
+      const localidadesNOErmita = props.comunidades.filter(it => it.ermita === null).map(it => {
+        if (it.ubicacion === null) {
+          return (
+            <div className={classes.DataContainer}>
+              <h5>Comunidad {it.nombre_de_comunidad} - Región {it.nombre_region}</h5>
             </div>
           )
         }
@@ -178,6 +88,13 @@ const Sandbox = (props) => {
 
       setLocalidades(localidadesWithGeo)
       setLocalSinGeo(localidadesNOGeo)
+      setLocalSinErmita(localidadesNOErmita)
+      setCuenta({
+          total: props.comunidades.length,
+          asignadas: props.comunidades.length - localidadesNOGeo.length - localidadesNOErmita.length,
+          sinErmita: localidadesNOErmita.length,
+          sinInegi: localidadesNOGeo.length
+        })
     }
   }, [props.comunidades, props.regiones])
 
@@ -185,7 +102,7 @@ const Sandbox = (props) => {
   useEffect(() => {
      setRegiones(regionData.map(r => {
        return (
-             <Polygon color={r.color} positions={r.coordinates}>
+             <Polygon key={r.id} color={r.color} positions={r.coordinates}>
                <Tooltip>
                  <p>{r.name}</p>
                </Tooltip>
@@ -211,8 +128,14 @@ const Sandbox = (props) => {
         </LayersControl>
 
       </Map>
-      <h2 className={classes.Subtitle}>Localidades Ubicadas {localidades && localidades.length}</h2>
-      <h2 className={classes.Subtitle}>Localidades sin Ubicar {localSinGeo && localSinGeo.length}</h2>
+      <h2 className={classes.Subtitle}>Comunidades Ubicadas {cuenta.asignadas}</h2>
+      <h2 className={classes.Subtitle}>Comunidades Sin Ermita {cuenta.sinErmita}</h2>
+      <h2 className={classes.Subtitle}>Comunidades Sin INEGI {cuenta.sinInegi}</h2>
+      <hr/>
+      <h2 className={classes.Subtitle}>LISTADO de Comunidades Sin Ermita</h2>
+      {localSinErmita}
+      <hr/>
+      <h2 className={classes.Subtitle}>LISTADO de Comunidades sin INEGI</h2>
       {localSinGeo}
     </div>
   );
---
+++ src/containers/Panel/Mapas/Sandbox/icons.js
@@ -0,0 +1,107 @@
+import r1 from "../../../../assets/Icons/Regions/placeholder.1.svg";
+import r2 from "../../../../assets/Icons/Regions/placeholder.2.svg";
+import r3 from "../../../../assets/Icons/Regions/placeholder.3.svg";
+import r4 from "../../../../assets/Icons/Regions/placeholder.4.svg";
+import r5 from "../../../../assets/Icons/Regions/placeholder.5.svg";
+import r6 from "../../../../assets/Icons/Regions/placeholder.6.svg";
+import r7 from "../../../../assets/Icons/Regions/placeholder.7.svg";
+import r8 from "../../../../assets/Icons/Regions/placeholder.8.svg";
+import r9 from "../../../../assets/Icons/Regions/placeholder.9.svg";
+import r10 from "../../../../assets/Icons/Regions/placeholder.10.svg";
+import L from 'leaflet'
+
+
+export const icons = {
+  '1': new L.Icon({
+    iconUrl:  r1,
+    iconRetinaUrl: r1,
+    iconAnchor: [5, 55],
+    popupAnchor: [10, -44],
+    iconSize: [25, 55],
+    shadowSize: [68, 95],
+    shadowAnchor: [20, 92],
+  }),
+  '2': new L.Icon({
+    iconUrl:  r2,
+    iconRetinaUrl: r2,
+    iconAnchor: [5, 55],
+    popupAnchor: [10, -44],
+    iconSize: [25, 55],
+    shadowSize: [68, 95],
+    shadowAnchor: [20, 92],
+  }),
+  '3': new L.Icon({
+    iconUrl:  r3,
+    iconRetinaUrl: r3,
+    iconAnchor: [5, 55],
+    popupAnchor: [10, -44],
+    iconSize: [25, 55],
+    shadowSize: [68, 95],
+    shadowAnchor: [20, 92],
+  }),
+  '4': new L.Icon({
+    iconUrl:  r4,
+    iconRetinaUrl: r4,
+    iconAnchor: [5, 55],
+    popupAnchor: [10, -44],
+    iconSize: [25, 55],
+    shadowSize: [68, 95],
+    shadowAnchor: [20, 92],
+  }),
+  '5': new L.Icon({
+    iconUrl:  r5,
+    iconRetinaUrl: r5,
+    iconAnchor: [5, 55],
+    popupAnchor: [10, -44],
+    iconSize: [25, 55],
+    shadowSize: [68, 95],
+    shadowAnchor: [20, 92],
+  }),
+  '6': new L.Icon({
+    iconUrl:  r6,
+    iconRetinaUrl: r6,
+    iconAnchor: [5, 55],
+    popupAnchor: [10, -44],
+    iconSize: [25, 55],
+    shadowSize: [68, 95],
+    shadowAnchor: [20, 92],
+  }),
+  '7': new L.Icon({
+    iconUrl:  r7,
+    iconRetinaUrl: r7,
+    iconAnchor: [5, 55],
+    popupAnchor: [10, -44],
+    iconSize: [25, 55],
+    shadowSize: [68, 95],
+    shadowAnchor: [20, 92],
+  }),
+  '8': new L.Icon({
+    iconUrl:  r8,
+    iconRetinaUrl: r8,
+    iconAnchor: [5, 55],
+    popupAnchor: [10, -44],
+    iconSize: [25, 55],
+    shadowSize: [68, 95],
+    shadowAnchor: [20, 92],
+  }),
+  '9': new L.Icon({
+    iconUrl:  r9,
+    iconRetinaUrl: r9,
+    iconAnchor: [5, 55],
+    popupAnchor: [10, -44],
+    iconSize: [25, 55],
+    shadowSize: [68, 95],
+    shadowAnchor: [20, 92],
+  }),
+  '10': new L.Icon({
+    iconUrl:  r10,
+    iconRetinaUrl: r10,
+    iconAnchor: [5, 55],
+    popupAnchor: [10, -44],
+    iconSize: [25, 55],
+    shadowSize: [68, 95],
+    shadowAnchor: [20, 92],
+  }),
+}
+
+export default icons
