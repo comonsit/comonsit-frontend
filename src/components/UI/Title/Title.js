726d6b838ee3205af4d3e7de27d156910231d1ed
mauricioinaz <mauricio.glez@gmail.com>
2020-11-11 16:40:09 -0500

    RE:Title: Style fixes

--- src/components/UI/Title/Title.js
+++ src/components/UI/Title/Title.js
@@ -6,7 +6,7 @@ import classes from './Title.module.scss';
 
 const title = (props) => (
   <div className={classes.HeaderContainer}>
-    <div className={classes.HeaderContainer_main}>
+    <div className={[classes.HeaderContainer_main, classes[props.titleType]].join(' ')}>
       <h1><FormattedMessage id={props.titleName}/>{props.titleNameEx}</h1>
     </div>
     <div className={classes.HeaderContainer_extra}>
--- src/containers/Panel/Socios/SociosForm/SociosForm.js
+++ src/containers/Panel/Socios/SociosForm/SociosForm.js
@@ -639,6 +639,7 @@ class SociosForm extends Component {
         <Title
           titleName="socioForm.title"
           titleNameEx={": " + this.props.selSocio.clave_socio}
+          titleType="ModalTitle"
         >
           {editButton}
         </Title>
